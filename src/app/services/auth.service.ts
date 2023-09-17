c4e8a21a3b7e33e98d8a90c440dba79441677dc4
Alejandro Gomez <alegomezc98@icloud.com>
2023-09-17 10:18:40 -0600

    I added some function to the auth.service to log in with google and create a user from my website, I also created the user interface to create a save users

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
+import { GoogleAuthProvider } from '@angular/fire/auth';
+import { User } from '../interfaces/user';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +12,40 @@ import { Router } from '@angular/router';
 export class AuthService {
 
   private isTMDBAuthenticated = false;
-  // public readonly userCredentials = {} | undefined;
+  public userCredentials!: User;
+  private GoogleProvider = new GoogleAuthProvider();
 
   constructor(
     private fbAuth: AngularFireAuth,
+    private storage: AngularFireStorage,
     private route: Router
-  ) { }
+  ) {}
+
+  GoogleAuth() {
+    this.fbAuth.signInWithPopup(this.GoogleProvider).then((res) => {
+      this.userCredentials = {
+        displayName: res.user!.displayName,
+        email: res.user!.email,
+        emailVerified: res.user!.emailVerified,
+        isAnonymous: res.user!.isAnonymous,
+        metadata: res.user!.metadata,
+        phoneNumber: res.user!.phoneNumber,
+        photoURL: res.user!.photoURL,
+        providerData: res.user!.providerData,
+        providerId: res.user!.providerId,
+        refreshToken: res.user!.refreshToken,
+        tenantId: res.user!.tenantId,
+        uid: res.user!.uid,
+      };
+
+      this.route.navigate(['./Home']);
+
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+
 
   setAuthenticated(value: boolean) {
     this.isTMDBAuthenticated = value;
@@ -31,6 +63,19 @@ export class AuthService {
     this.fbAuth.signOut().then(() => {
       this.route.navigate(['./Login']);
     });
+  }
+
+  async register(firstname: string, lastname: string, email: string, phone: string, password: string, profile: File): Promise<void> {
+
+    // const regStatus = await this.fbAuth.createUserWithEmailAndPassword(email, password);
+
+    // if (regStatus) {
+    //   regStatus.user!.displayName = `${firstname} ${lastname}`;
+    //   regStatus.user!.phoneNumber = phone;
+    // }
+
+    // console.log(this.storage);
+
   }
 
 }
